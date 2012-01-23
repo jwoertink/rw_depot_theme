860a2a6a53d11ce6dbf7f536f9cc84be010e65f7
Jeremy Woertink <jeremywoertink@gmail.com>
2012-01-23 10:33:04 -0800

    running a test with the name change

--- lib/generators/rw_depot_theme/install/install_generator.rb
+++ lib/generators/rw_depot_theme/install/install_generator.rb
@@ -2,16 +2,20 @@ require 'rails'
 
 module RwDepotTheme
   module Generators
-    class SetupGenerator < ::Rails::Generators::Base
-      
-      desc "This generator will create all the views for the depot front-end"
+    class InstallGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../../../../app/views", __FILE__)
       
-      def setup
+      desc "This generator will create all the views for the depot front-end"
+      
+      def copy_layout
         say_status("copying", "Application Layout", :green)
         remove_file("app/views/layouts/application.html.erb")
         copy_file("application.html.erb", "app/views/layouts/application.html.erb")
-        
+      end
+      
+      def add_asset_loads
+        append_file("apps/assets/javascripts/application.js", "//= require depot")
+        inject_into_file("apps/assets/stylesheets/application.css", :before => '*/', " *= require depot")
       end
     end
   end
